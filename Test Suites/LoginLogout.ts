91439e98c8ce3386032e785b41e72477e219cccd
Jeffrey Espinili <jeffrey.espinili@adslot.com>
2018-05-21 15:30:14 +1000

    Added Test Suite

---
+++ Test Suites/LoginLogout.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginLogout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-21T14:54:31</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e38acd6d-735c-40f8-81b7-dc41b3ebfef8</testSuiteGuid>
+   <testCaseLink>
+      <guid>db0d92eb-62c0-456b-96c6-973213615a17</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginLogout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
